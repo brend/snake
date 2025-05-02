52a65eb5c57a14750e79410558ab63da2b46380b
waldrumpus <waldrumpus@gmail.com>
2025-05-02 21:30:52 +0200

    remove all drawing code

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,77 @@
+use std::ops::AddAssign;
 use std::vec;
+use neural_network_study::NeuralNetwork;
+use rand::prelude::*;
+use rand::rngs::StdRng;
 
-use macroquad::prelude::*;
-
-const W: f32 = 30.0;
 const ROWS: usize = 20;
 const COLS: usize = 20;
-const INITIAL_SPEED: f32 = 0.5;
 
-fn random_position() -> Vec2 {
-    let x = rand::gen_range(0, ROWS) as f32 * W;
-    let y = rand::gen_range(0, COLS) as f32 * W;
-    Vec2::new(x, y)
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Pos {
+    x: i32,
+    y: i32,
 }
 
-fn snap(x: f32) -> f32 {
-    (x / W).floor() * W
+impl Pos {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    fn random(rng: &mut StdRng) -> Self {
+        let x = rng.random_range(0..COLS as i32);
+        let y = rng.random_range(0..ROWS as i32);
+        Self::new(x, y)
+    }
 }
 
-fn snapv(v: &Vec2) -> Vec2 {
-    Vec2::new(snap(v.x), snap(v.y))
+impl AddAssign<Dir> for Pos {
+    fn add_assign(&mut self, dir: Dir) {
+        match dir {
+            Dir::Horizontal(x) => self.x = self.x + x,
+            Dir::Vertical(y) => self.y = self.y + y,
+        }
+    }
+}
+
+impl Dir {
+    fn up() -> Self {
+        Self::Vertical(-1)
+    }
+
+    fn down() -> Self {
+        Self::Vertical(1)
+    }
+
+    fn left() -> Self {
+        Self::Horizontal(-1)
+    }
+
+    fn right() -> Self {
+        Self::Horizontal(1)
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Dir {
+    Horizontal(i32),
+    Vertical(i32),
 }
 
 struct Snake {
-    body: Vec<Vec2>,
-    velocity: Vec2,
-    speed: f32,
+    body: Vec<Pos>,
+    direction: Dir,
 }
 
 impl Snake {
     fn new() -> Self {
         Self {
-            body: vec![Vec2::new(0.0, W * 2.0)],
-            velocity: Vec2::new(INITIAL_SPEED, 0.0),
-            speed: INITIAL_SPEED,
+            body: vec![Pos::new(0, 2)],
+            direction: Dir::Horizontal(1),
         }
     }
 
-    fn head(&self) -> Vec2 {
+    fn head(&self) -> Pos {
         self.body[0]
     }
 
@@ -47,41 +82,23 @@ impl Snake {
     fn grow(&mut self) {
         // Add a new segment to the snake's body
         let last_segment = self.body.last().unwrap();
-        self.body.push(snapv(last_segment));
+        self.body.push(*last_segment);
     }
 
-    fn speed_up(&mut self) {
-        // Increase the snake's speed
-        self.speed += 0.1;
-        self.velocity = self.velocity.normalize() * self.speed;
-    }
-
-    fn update(&mut self) -> bool {
+    fn update(&mut self) {
         // Move the snake by updating the position of the head
-        let head_on_grid = snapv(&self.head());
-        self.body[0] += self.velocity;
-        if head_on_grid != snapv(&self.head()) {
-            // If the head has moved to a new grid cell, update the body
-            for i in (1..self.body.len()).rev() {
-                self.body[i] = self.body[i - 1];
-            }
-            return true;
-        }
-        false
-    }
-
-    fn draw(&self) {
-        for segment in &self.body {
-            draw_rectangle(snap(segment.x), snap(segment.y), W, W, BLACK);
+        self.body[0] += self.direction;
+        // Move the rest of the body
+        for i in (1..self.body.len()).rev() {
+            self.body[i] = self.body[i - 1];
         }
     }
 
-    fn eat(&mut self, food: &Vec2) -> bool {
+    fn eat(&mut self, food: Pos) -> bool {
         // Check if the snake's head is on the food
-        if snapv(&self.head()) == snapv(food) {
+        if self.head() == food {
             // If so, grow the snake and return true
             self.grow();
-            self.speed_up();
             return true;
         }
         false
@@ -94,105 +111,92 @@ enum GameState {
     GameOver,
 }
 
-struct Brain {}
+#[derive(Clone)]
+struct Brain {
+    nn: NeuralNetwork,
+}
 
 impl Brain {
-    fn make_move(&self, input: &Vec<f64>) -> Vec<f64> {
-        let snake_x = input[0];
-        let snake_y = input[1];
-        let snake_vx = input[2];
-        let snake_vy = input[3];
-        let food_x = input[4];
-        let food_y = input[5];
-        let dx = food_x - snake_x;
-        let dy = food_y - snake_y;
-        // move towards the food
-        if dx.abs() > dy.abs() {
-            if dx > 0.0 {
-                return vec![0.0, 0.0, 0.0, 1.0]; // Move right
-            } else {
-                return vec![0.0, 0.0, 1.0, 0.0]; // Move left
-            }
-        } else {
-            if dy > 0.0 {
-                return vec![0.0, 1.0, 0.0, 0.0]; // Move down
-            } else {
-                return vec![1.0, 0.0, 0.0, 0.0]; // Move up
-            }
+    fn new() -> Self {
+        Self {
+            nn: NeuralNetwork::new(4, 6, 4),
         }
+    }
+
+    fn make_move(&self, input: &Vec<f64>) -> Vec<f64> {
+        // Use the neural network to make a move
+        let output = self.nn.predict(input.clone());
+        output
+    }
+
+    fn mutate(&mut self) {
+        self.nn.mutate(0.1);
     }
 }
 
 struct Game {
     state: GameState,
-    food: Vec2,
+    food: Pos,
     snake: Snake,
     brain: Brain,
+    rng: StdRng,
 }
 
 impl Game {
     fn new(brain: Brain) -> Self {
+        let mut rng = StdRng::from_seed([42u8; 32]);
         Self {
             state: GameState::Running,
-            food: random_position(),
+            food: Pos::random(&mut rng),
             snake: Snake::new(),
             brain,
+            rng,
         }
     }
 
     fn update(&mut self) {
         match self.state {
-            GameState::GameOver => {
-                if is_key_pressed(KeyCode::Space) {
-                    // Restart the game
-                    self.snake = Snake::new();
-                    self.food = random_position();
-                    self.state = GameState::Running;
-                }
-            }
+            GameState::GameOver => {}
             GameState::Running => {
-                if self.snake.update() {
-                    // Check for collision with itself
-                    for i in 2..self.snake.len() {
-                        if snapv(&self.snake.head()) == snapv(&self.snake.body[i]) {
-                            // Game over
-                            self.state = GameState::GameOver;
-                            break;
-                        }
+                self.snake.update();
+                // Check for collision with itself
+                for i in 2..self.snake.len() {
+                    if self.snake.head() == self.snake.body[i] {
+                        // Game over
+                        self.state = GameState::GameOver;
+                        break;
                     }
+                }
 
-                    // Get the next move from the brain
-                    let input = vec![
-                        snap(self.snake.head().x) as f64,
-                        snap(self.snake.head().y) as f64,
-                        self.snake.velocity.x as f64,
-                        self.snake.velocity.y as f64,
-                        self.food.x as f64,
-                        self.food.y as f64,
-                    ];
-                    let output = self.brain.make_move(&input);
-                    // Update the snake's velocity based on the brain's output
-                    if output[0] > 0.5 {
-                        self.snake.velocity = Vec2::new(0.0, -self.snake.speed);
-                    } else if output[1] > 0.5 {
-                        self.snake.velocity = Vec2::new(0.0, self.snake.speed);
-                    } else if output[2] > 0.5 {
-                        self.snake.velocity = Vec2::new(-self.snake.speed, 0.0);
-                    } else if output[3] > 0.5 {
-                        self.snake.velocity = Vec2::new(self.snake.speed, 0.0);
-                    }
+                // Get the next move from the brain
+                let input = vec![
+                    self.snake.head().x as f64 / COLS as f64,
+                    self.snake.head().y as f64 / ROWS as f64,
+                    self.food.x as f64 / COLS as f64,
+                    self.food.y as f64 / ROWS as f64,
+                ];
+                let output = self.brain.make_move(&input);
+                // Update the snake's direction based on the brain's output
+                if output[0] > 0.5 {
+                    self.snake.direction = Dir::up();
+                } else if output[1] > 0.5 {
+                    self.snake.direction = Dir::down();
+                } else if output[2] > 0.5 {
+                    self.snake.direction = Dir::left();
+                } else if output[3] > 0.5 {
+                    self.snake.direction = Dir::right();
                 }
 
                 // Check for collision with food
-                if self.snake.eat(&self.food) {
-                    self.food = random_position();
+                if self.snake.eat(self.food) {
+                    self.food = Pos::random(&mut self.rng);
                 }
 
                 // Check for collision with walls
-                if self.snake.head().x < 0.0
-                    || self.snake.head().x >= W * COLS as f32
-                    || self.snake.head().y < 0.0
-                    || self.snake.head().y >= W * ROWS as f32
+                if self.snake.head().x < 0
+                    || self.snake.head().x >= COLS as i32
+                    || self.snake.head().y < 0
+                    || self.snake.head().y >= ROWS as i32
                 {
                     // Game over
                     self.state = GameState::GameOver;
@@ -201,64 +205,94 @@ impl Game {
         }
     }
 
-    fn draw(&self) {
-        let food_color = Color::from_hex(0x355834);
-        let grid_colors = vec![Color::from_hex(0xDEC0F1), Color::from_hex(0xB79CED)];
-        let font_size = 40.0;
-    
-        // Draw the grid
-        for y in 0..ROWS {
-            for x in 0..COLS {
-                let color = grid_colors[(x + y) % 2];
-                draw_rectangle(x as f32 * W, y as f32 * W, W, W, color);
-            }
-        }
-
-        // Draw the food
-        draw_rectangle(self.food.x.floor(), self.food.y.floor(), W, W, food_color);
-
-        // Draw the snake
-        self.snake.draw();
-
-        // Print score (snake length)
-        let score = self.snake.body.len();
-        draw_text(&format!("Score: {}", score), 10.0, 20.0, font_size, BLACK);
-
-        match self.state {
-            GameState::GameOver => {
-                let game_over_message = format!("Game Over! Score: {}. Press Space to Restart", self.evaluate());
-                draw_text(
-                    &game_over_message,
-                    10.0,
-                    50.0,
-                    font_size,
-                    RED,
-                );
-            }
-            _ => {}
-        }
-    }
-
     fn evaluate(&self) -> f32 {
         // Evaluate the snake's performance
         let dx = self.snake.head().x - self.food.x;
         let dy = self.snake.head().y - self.food.y;
         let distance_to_food = dx.abs() + dy.abs();
-        let score = self.snake.len() as f32  * 100.0;
-        (score - distance_to_food + (ROWS * COLS) as f32) / 10000.0
+        (self.snake.len() as i32 * 100 - distance_to_food + (ROWS as i32 + COLS as i32)) as f32
     }
 }
 
-#[macroquad::main("Snake Game")]
-async fn main() {
-    let mut game = Game::new(Brain {});
+fn main() {
+    let mut rng = StdRng::from_seed([42u8; 32]);
+    let mut generation = 0;
+    let population_size = 100;
+    let mut population = vec![];
+    for _ in 0..population_size {
+        population.push(Game::new(Brain::new()));
+    }
 
     loop {
-        game.update();
+        generation += 1;
+        println!("Generation: {}", generation);
 
-        clear_background(BLACK);
-        game.draw();
+        // DEBUG
+        if generation > 10 {
+            break;
+        }
 
-        next_frame().await;
+        // Run the simulation with the current population
+        // until all games are over
+        let max_steps = 1000;
+        let mut steps = 0;
+        loop {
+            steps += 1;
+            if steps > max_steps {
+                break;
+            }
+
+            let mut alive = false;
+            for game in &mut population {
+                game.update();
+                match game.state {
+                    GameState::Running => alive = true,
+                    GameState::GameOver => {}
+                }
+            }
+            if !alive {
+                break;
+            }
+        }
+
+        // Evaluate the population and create a mating pool
+        let mut mating_pool = vec![];
+        let mut scored_games = population.iter().map(|g| (g.evaluate(), g)).collect::<Vec<_>>();
+        scored_games.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+        let mut score_sum = 0.0;
+        let mut best_score = 0.0;
+        for (score, game) in &scored_games {
+            score_sum += score;
+            if score > &best_score {
+                best_score = *score;
+            }
+            mating_pool.push(game);
+        }
+
+        println!("Best score: {}", best_score);
+        println!("Average score: {}", score_sum / population_size as f32);
+
+        // Create a new generation
+        let mut new_population = vec![];
+        for _ in 0..population_size {
+            // Randomly select a parent from the mating pool
+            let r = rng.random_range(0.0..score_sum);
+            let mut cumulative_score = 0.0;
+            let mut selected_parent = None;
+            for (score, game) in &scored_games {
+                cumulative_score += score;
+                if cumulative_score >= r {
+                    selected_parent = Some(game);
+                    break;
+                }
+            }
+            let selected_parent = selected_parent.unwrap();
+            // Apply some mutation to the parent's brain
+            let mut child_brain = selected_parent.brain.clone();
+            child_brain.mutate();
+            // Create a new child by combining the parents' brains
+            new_population.push(Game::new(child_brain));
+        }
+        population = new_population;
     }
 }
